ba0fb27c043d6f93d11cf6ab97119a0c7667310a
laurenwden <laurenm.weber@outlook.com>
2020-06-05 16:18:39 -0600

    route for cases and all forms. -LW

--- flask_nza/routes.py
+++ flask_nza/routes.py
@@ -6,3 +6,32 @@ from flask import render_template, request, redirect, url_for
 #from flask_nza.models import User, Post, check_password_hash
 
 from flask_login import login_required,login_user,current_user,logout_user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/cases', methods=['GET', 'POST'])
+@login_required
+def case():
+    case = CaseForm()
+    if request.method == 'POST' and post.validate():
+        title = case.title.data
+        note = case.note.data
+        user_id = current_user.id
+        print("\n", title, note)
+        case = Case(title, note, user_id)
+        db.session.add(case)
+        db.session.commit()
+        return redirect(url_for('cases'))#clears out the form fields
+    return render_template('cases.html', post=post)
