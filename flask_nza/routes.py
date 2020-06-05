6d9b468a7b332bbb057c19c08248c327836ef121
celestebarrios <49879625+celestebarrios@users.noreply.github.com>
2020-06-05 17:58:11 -0500

    Register/login/logout

--- flask_nza/routes.py
+++ flask_nza/routes.py
@@ -7,7 +7,7 @@ from flask import render_template, request, redirect, url_for
 
 from flask_login import login_required,login_user,current_user,logout_user
 
-# Register Route
+#Register Route
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = UserInfoForm()
@@ -23,23 +23,9 @@ def register():
         db.session.add(user)
         # Save info into database
         db.session.commit()
-
-        # Flask Email Sender 
-        msg = Message(f'Thanks for Signing Up! {email}', recipients=[email])
-        msg.body = ('Congrats on signing up! Looking forward to your posts!')
-        msg.html = ('<h1> Welcome to May_Blog!</h1>' '<p> This will be fun! </p>')
-
-        mail.send(msg)
     return render_template('register.html',form = form)
-@app.route('/posts/delete/<int:post_id>', methods=['POST'])
-@login_required
-def post_delete(post_id):
-    post = Post.query.get_or_404(post_id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect(url_for('home'))
 
-# Login Form Route
+#Login
 @app.route('/login', methods = ['GET','POST'])
 def login():
     form = LoginForm()
@@ -55,7 +41,7 @@ def login():
 
     return render_template('login.html',form = form)
 
-
+#Logout
 @app.route('/logout')
 @login_required
 def logout():
