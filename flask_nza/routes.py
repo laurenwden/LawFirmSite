96ec6c87fca4d070f610fe79e9a96a8c1dd694cf
celestebarrios <49879625+celestebarrios@users.noreply.github.com>
2020-06-05 17:29:26 -0500

    forms

--- flask_nza/routes.py
+++ flask_nza/routes.py
@@ -6,3 +6,60 @@ from flask import render_template, request, redirect, url_for
 #from flask_nza.models import User, Post, check_password_hash
 
 from flask_login import login_required,login_user,current_user,logout_user
+
+# Register Route
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        # Get Information
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        print("\n",username,password,email)
+        # Create an instance of User
+        user = User(username,email,password)
+        # Open and insert into database
+        db.session.add(user)
+        # Save info into database
+        db.session.commit()
+
+        # Flask Email Sender 
+        msg = Message(f'Thanks for Signing Up! {email}', recipients=[email])
+        msg.body = ('Congrats on signing up! Looking forward to your posts!')
+        msg.html = ('<h1> Welcome to May_Blog!</h1>' '<p> This will be fun! </p>')
+
+        mail.send(msg)
+    return render_template('register.html',form = form)
+@app.route('/posts/delete/<int:post_id>', methods=['POST'])
+@login_required
+def post_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+# Login Form Route
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+        logged_user = User.query.filter(User.email == email).first()
+        if logged_user and check_password_hash(logged_user.password, password):
+            login_user(logged_user)
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+
+    return render_template('login.html',form = form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
