6daed03a2278c08f269ce8c465c60ed1a410be62
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-05 17:11:03 -0500

    delete route & html v0

--- flask_nza/routes.py
+++ flask_nza/routes.py
@@ -6,3 +6,11 @@ from flask import render_template, request, redirect, url_for
 #from flask_nza.models import User, Post, check_password_hash
 
 from flask_login import login_required,login_user,current_user,logout_user
+
+@app.route('/cases/delete/<int:post_id>', methods=['POST'])
+@login_required
+def case_delete(post_id):
+    case = Post.query.get_or_404(case_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('home'))
